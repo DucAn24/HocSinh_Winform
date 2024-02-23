a5d7ac3f2cf79159e4bbd99349c337d026794cc0
DucAn <annguyen2422004@gmail.com>
2024-02-23 17:46:19 +0700

    create GiangVien form

---
+++ GiangVien.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thucHanh
+{
+    internal class GiangVien: People
+    {
+        public GiangVien(String ten, String diachi, String cmnd, DateTime ngaysinh) : base(ten, diachi, cmnd, ngaysinh)
+        {
+
+        }
+    }
+}
---
+++ GiangVienDAO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thucHanh
+{
+    internal class GiangVienDAO
+    {
+        public void Add(GiangVien  giangVien)
+        {
+            string sqlStr = string.Format("INSERT INTO HocSinh(Ten, Diachi, Cmnd, NgayThangNam) VALUES ('{0}', '{1}', '{2}', GETDATE())", giangVien.getHoTen, giangVien.getDiaChi, giangVien.getCmnd, giangVien.getNgaySinh);
+            dbConnection db = new dbConnection();
+            db.Execute(sqlStr);
+        }
+
+        public void Modify(GiangVien giangVien)
+        {
+
+            string sqlStr = string.Format("UPDATE HocSinh SET Ten = '{0}', DiaChi = '{1}', NgayThangNam = '{2}'  WHERE CMND = '{3}' ", giangVien.getHoTen, giangVien.getDiaChi, giangVien.getNgaySinh, giangVien.getCmnd);
+            dbConnection db = new dbConnection();
+            db.Execute(sqlStr);
+        }
+
+        public void Delete(GiangVien giangVien)
+        {
+            string sqlStr = string.Format(" DELETE FROM HocSinh WHERE CMND = '{0}' ", giangVien.getCmnd);
+            dbConnection db = new dbConnection();
+            db.Execute(sqlStr);
+        }
+
+    }
+}
